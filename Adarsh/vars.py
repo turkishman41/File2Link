31596eb1436539fc059b9c83a0ad345302697471
turkishman41 <89349479+turkishman41@users.noreply.github.com>
2023-04-14 16:01:14 +0330

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'postgres://qketcquh:hY0-zPecRp78ag711jMUmKK_t1aYVHzQ@abul.db.elephantsql.com/qketcquh'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Turkic:Turkic@turkic.owgjdvr.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'dto_bots'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
